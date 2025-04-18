8d70675e4c2a6dfce6ccbdcd0e66864fc08f89a3
Samuel Escalera <Samuel.Escalera@fundacion-jala.org>
2025-04-18 19:00:58 -0400

    feat: save mark place

--- app/src/main/java/com/example/exploralocal/MapsActivity.kt
+++ app/src/main/java/com/example/exploralocal/MapsActivity.kt
@@ -5,8 +5,10 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,6 +17,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.exploralocal.databinding.ActivityMapsBinding
+import com.example.exploralocal.db.Place
 import com.example.exploralocal.viewmodels.PlacesViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -64,6 +67,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.uiSettings.isZoomControlsEnabled = true
         mMap.uiSettings.isMyLocationButtonEnabled = true
 
+        mMap.setOnMapClickListener { latLng ->
+            showAddPlaceDialog(latLng)
+        }
         viewModel.places.observe(this) { places ->
             places.forEach { place ->
                 val location = LatLng(place.latitude, place.longitude)
@@ -79,6 +85,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         viewModel.loadPlaces()
     }
 
+    private fun showAddPlaceDialog(latLng: LatLng) {
+        val dialogView = layoutInflater.inflate((R.layout.dialog_add_place), null)
+        val nameInput = dialogView.findViewById<EditText>(R.id.etPlaceName)
+        val descInput = dialogView.findViewById<EditText>(R.id.etPlaceDescription)
+        val ratingInput = dialogView.findViewById<EditText>(R.id.etPlaceRating)
+
+        AlertDialog.Builder(this)
+            .setTitle("Agregar nuevo lugar")
+            .setView(dialogView)
+            .setPositiveButton("Guardar") { _, _, ->
+                val name = nameInput.text.toString()
+                val description = descInput.text.toString()
+                val rating = ratingInput.text.toString().toFloatOrNull() ?: 0f
+
+                if (name.isNotBlank()) {
+                    val newPlace = Place(
+                        name = name,
+                        description = description,
+                        rating = rating,
+                        latitude = latLng.latitude,
+                        longitude = latLng.longitude,
+                        photoPath = null
+                    )
+                    viewModel.addPlace(newPlace)
+                }
+            }
+            .setNegativeButton("Cancelar", null)
+            .show()
+    }
     private fun checkLocationPermission() {
         when {
             ContextCompat.checkSelfPermission(
